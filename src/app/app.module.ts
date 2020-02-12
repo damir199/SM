24f80637949861e6efa85c78e3f08b1b98dd9375
damir pasic <damirpasic@damirs-MacBook-Pro.local>
2020-02-12 17:01:01 +0000

    initial component created and tested. stream-create, stream-view created. app-module is edited for initial start.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {StreamCreateComponent} from './Streams/stream-create/stream-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StreamCreateComponent
   ],
   imports: [
     BrowserModule,
